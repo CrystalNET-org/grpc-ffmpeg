3646fee9b47a0b658ded4c5af46148f6cb63c4b8
Lukas Wingerberg <h@xx0r.eu>
2026-02-06 14:38:35 +0100

    lets to the fancy thing and re-implement the client in rust

--- src/client/rust/build.rs
+++ src/client/rust/build.rs
@@ -1,4 +1,9 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../proto/ffmpeg.proto")?;
+fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    tonic_prost_build::configure()
+        .build_server(false) // We only need to generate client code
+        .compile_protos(
+            &["../../proto/ffmpeg.proto"],
+            &["../../proto"], // Include the directory where ffmpeg.proto is located
+        )?;
     Ok(())
 }
